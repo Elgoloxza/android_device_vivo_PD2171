5f1810751e557cb6c398267d62b7467e785e3c18
Elgoloxza <150365503+Elgoloxza@users.noreply.github.com>
2023-11-19 04:25:43 +0800

    Update

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,12 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-COMMON_LUNCH_CHOICES omni_PD2171-user
-COMMON_LUNCH_CHOICES omni_PD2171-userdebug
-COMMON_LUNCH_CHOICES omni_PD2171-eng
+<<<<<<< HEAD
+add_lunch_combo omni_PD2171-user
+add_lunch_combo omni_PD2171-userdebug
+add_lunch_combo omni_PD2171-eng
+=======
+common_lunch_choices omni_PD2171-user
+common_lunch_choices omni_PD2171-userdebug
+common_lunch_choices omni_PD2171-eng
+>>>>>>> 3c7a544a4b2f3dea1e4a57b382a5553e6c63b811
